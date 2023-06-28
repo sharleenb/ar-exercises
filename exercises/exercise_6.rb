eb5e279036e5de72f4ac2df9b0f8f788c7520cfd
Sharleen Beniamin <sharleenb16@gmail.com>
2023-06-28 18:01:11 +0000

    add employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,27 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store 
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 50)
+
+@store1.employees.create(first_name: "Sharleen", last_name: "Beniamin", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Shana", last_name: "Virani", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Bob", last_name: "Beniamin", hourly_rate: 30)
+
+@store4.employees.create(first_name: "Violet", last_name: "Smith", hourly_rate: 10)
+
+@store4.employees.create(first_name: "Sandy", last_name: "Beniamin", hourly_rate: 60)
+
+@store6.employees.create(first_name: "Kevin", last_name: "Dale", hourly_rate: 80)
+
+@store6.employees.create(first_name: "Mary", last_name: "Howard", hourly_rate: 60)
